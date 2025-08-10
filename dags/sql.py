41b11452b82cdb4c5cc599f23d8d42a450a5f4f3
shahan360 <sbsb4961@gmail.com>
2025-08-10 10:14:18 -0500

    complete commit

---
+++ dags/sql.py
@@ -0,0 +1,14 @@
+from airflow.sdk import dag, task
+
+@dag
+def sql_dag():
+
+    @task.sql(
+        conn_id="postgres"
+    )
+    def get_nb_xcoms():
+        return "SELECT COUNT(*) FROM xcom"
+
+    get_nb_xcoms()
+
+sql_dag()
